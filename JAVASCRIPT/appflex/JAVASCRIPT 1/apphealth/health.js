0548e3a4144f3cb19d14e4a4270221973af76c29
Paulo Nicolas <115593922+paulonicolas5569@users.noreply.github.com>
2023-11-01 22:08:52 -0300

    Update health.js

--- JAVASCRIPT/appflex/JAVASCRIPT 1/apphealth/health.js
+++ JAVASCRIPT/appflex/JAVASCRIPT 1/apphealth/health.js
@@ -1,10 +1,55 @@
-/**
- * App Health
- * @author Paulo Nicolas 
- */
+<!DOCTYPE html>
+<html lang="pt-br">
 
-function calcularIMC() {
-    alert("teste")
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>APP - Health</title>
+</head>
 
+<body>
+    <div id="main">
+        <h1>APP - Health</h1>
+        <form name="frmIMC">
+            <input type="number" name="txtIdade" placeholder="Idade" class="caixa">
+            <input type="number" name="txtPeso" placeholder="Peso" class="caixa">
+            <input type="number" name="txtAltura" placeholder="Altura" class="caixa">
+            <p>
+                sexo:
+                <input type="radio" name="grupo" id="m">masulino
+                <input type="radio" name="grupo" id="f">Femenino
 
+            </p>
+            <p>
+                <select name="nivel" id="atividade">
+                    <option value="">Selecione o nivel de atividade fisica </option>
+                    <option value="1.2">Sedentario (trabalho de scritorio)</option>
+                    <option value="1.375">Exercicio Leve (1 a 2 dia por semana )</option>
+                    <option value="1.55">Exercicio Moderado (3 a 4 dias por semana)</option>
+                    <option value="1.725">Exercicio pesado (6 a 7 dias por semana)</option>
+                    <option value="1.9">Ativo (7 dias ou mais por semana)</option>
+                </select>
+            </p>
+            <P>
+                <input type="button" value="Calcular" onclick="calcular()">
+            <input type="reset" value="Limpar" onclick="limpar()">
+            </P>
+        </form>
+        <img src="icons/reset.png" alt="marcador IMC" id="grafico">
+        <h3 id="imc"></h3>
+        <h4 id="status"></h4>
+
+        <img src="icons/heart.png" alt="coração" id="grafico">
+        <h3 id="freq"> </h3>
+        <h3>Calorias por dia: </h3> 
+        <hr>
+        <h4 id="calorias"></h4>
+        
+    </div>
+    <script src="health.js"></script>
+
+</body>
+
+</html>
